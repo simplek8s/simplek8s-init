c553b88f35e19d54f52c9524c0ae0a4a5f24a6da
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2022-12-15 18:20:33 +0100

    src: test for common.CheckFileExists

--- internal/pkg/common/common_test.go
+++ internal/pkg/common/common_test.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -113,6 +114,80 @@ func TestIsDir(t *testing.T) {
 			got := IsDir(tt.dir)
 			if tt.want != got {
 				t.Errorf("want %v, got %v", tt.want, got)
+			}
+		})
+	}
+}
+
+func Test_isCmdlineDebug(t *testing.T) {
+	dir := t.TempDir()
+	for name, content := range map[string]string{
+		"fileJustDebug":       "debug",
+		"fileStart":           "debug after",
+		"fileBetween":         "before debug after",
+		"fileEnd":             "something debug",
+		"fileEmpty":           "",
+		"fileInAnotherCastle": "in another castle",
+	} {
+		if err := os.WriteFile(filepath.Join(dir, name), []byte(content), 0644); err != nil {
+			t.Fatal("can not write test files")
+		}
+	}
+
+	type args struct {
+		filePath string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "ok", args: args{filePath: filepath.Join(dir, "fileJustDebug")}, want: true},
+		{name: "start", args: args{filePath: filepath.Join(dir, "fileStart")}, want: true},
+		{name: "between", args: args{filePath: filepath.Join(dir, "fileBetween")}, want: true},
+		{name: "end", args: args{filePath: filepath.Join(dir, "fileEnd")}, want: true},
+		{name: "empty", args: args{filePath: filepath.Join(dir, "fileEmpty")}, want: false},
+		{name: "not-here", args: args{filePath: filepath.Join(dir, "fileInAnotherCastle")}, want: false},
+		{name: "file-not-found", args: args{filePath: filepath.Join(dir, "doesnotexists")}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isCmdlineDebug(tt.args.filePath); got != tt.want {
+				t.Errorf("isCmdlineDebug() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCheckFileExists(t *testing.T) {
+	dir := t.TempDir()
+	if err := os.WriteFile(filepath.Join(dir, "file"), []byte("I am a file"), 0644); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.WriteFile(filepath.Join(dir, "empty-file"), nil, 0644); err != nil {
+		t.Fatal(err)
+	}
+	if err := os.Mkdir(filepath.Join(dir, "directory"), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	type args struct {
+		filePath string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "ok-file", args: args{filePath: filepath.Join(dir, "file")}, want: true},
+		{name: "ok-empty-file", args: args{filePath: filepath.Join(dir, "empty-file")}, want: true},
+		{name: "ok-directory", args: args{filePath: filepath.Join(dir, "directory")}, want: true},
+		{name: "ko", args: args{filePath: filepath.Join(dir, "nothing")}, want: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CheckFileExists(tt.args.filePath); got != tt.want {
+				t.Errorf("CheckFileExists() = %v, want %v", got, tt.want)
 			}
 		})
 	}
