b7fb762ac869ff1c57d28f3fb767bbf287b0012a
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2022-12-15 19:08:59 +0100

    src: log to file and stdout

--- cmd/init/main.go
+++ cmd/init/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 	"strconv"
+	"time"
 
 	cmd "github.com/jlsalvador/simplek8s/internal/app/init"
 	"github.com/jlsalvador/simplek8s/pkg/common"
@@ -13,13 +16,24 @@ import (
 var Version string
 
 func showHelp() {
-	fmt.Printf("SimpleK8s %s\n", Version)
+	fmt.Println(Version)
 }
 
 func main() {
 	if debug, _ := strconv.ParseBool(common.GetEnv("DEBUG", "false")); debug || common.IsCmdlineDebug() {
 		log.SetLevel(log.DebugLevel)
 		log.SetReportCaller(true)
+
+		logFileName := fmt.Sprintf("simplek8s-init.%d.log", time.Now().Unix())
+		logFileName = filepath.Join(os.TempDir(), logFileName)
+		log.Infof("logfile: %s", logFileName)
+		if file, err := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644); err != nil {
+			log.Error(err)
+			panic(err)
+		} else {
+			defer file.Close()
+			log.SetOutput(io.MultiWriter(file, os.Stdout)) // Write into file and stdout
+		}
 	}
 	defer log.Debug("done")
 
