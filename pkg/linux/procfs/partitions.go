3488f7c2250ac865dbb09ce63f712b7d6bfac0e2
José Luis Salvador Rufo <salvador.joseluis@gmail.com>
2025-02-21 12:51:54 +0100

    src: catch ParseInt errors

--- pkg/linux/procfs/partitions.go
+++ pkg/linux/procfs/partitions.go
@@ -25,8 +25,7 @@ func ParsePartitions() ([]Partitions, error) {
 }
 
 func parsePartitions(procPartitionsFilePath string) ([]Partitions, error) {
-
-	// open partitions file
+	// Open the partitions file.
 	f, err := os.Open(procPartitionsFilePath)
 	if err != nil {
 		return nil, err
@@ -39,36 +38,44 @@ func parsePartitions(procPartitionsFilePath string) ([]Partitions, error) {
 		return nil, fmt.Errorf("%q is not a file", procPartitionsFilePath)
 	}
 
-	// for each line from file
+	// Parse each line from file.
 	partitions := []Partitions{}
 	s := bufio.NewScanner(f)
 	for i := 0; s.Scan(); i++ {
 		line := s.Text()
 
-		if i == 0 { // skip header
+		if i == 0 { // Skip header.
 			if line != "major minor  #blocks  name" {
 				return nil, fmt.Errorf("unexpected header, got %q", line)
 			}
 			continue
-		} else if i == 1 { // skip new line header separator
+		} else if i == 1 { // Skip "\n" header separator.
 			if line != "" {
 				return nil, fmt.Errorf("unexpected new line header separator, got %q", line)
 			}
 			continue
-
 		}
 
-		// parse fields
+		// Parse each fields.
 		fields := strings.Fields(line)
 		if len(fields) != 4 {
 			return nil, fmt.Errorf("can not parse line %q, from %q", line, procPartitionsFilePath)
 		}
-		major, _ := strconv.ParseInt(fields[0], 10, 64)
-		minor, _ := strconv.ParseInt(fields[1], 10, 64)
-		blocks, _ := strconv.ParseInt(fields[2], 10, 64)
+		major, err := strconv.ParseInt(fields[0], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		minor, err := strconv.ParseInt(fields[1], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		blocks, err := strconv.ParseInt(fields[2], 10, 64)
+		if err != nil {
+			return nil, err
+		}
 		name := fields[3]
 
-		// append new struct
+		// Append the parsed line as new struct.
 		partitions = append(partitions, Partitions{
 			Major:  major,
 			Minor:  minor,
